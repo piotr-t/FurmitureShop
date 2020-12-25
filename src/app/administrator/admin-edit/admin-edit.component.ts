f6b481319b70019d6e88b050af8549abe93429b4
piotr-t <troc.piotr@gmail.com>
2020-12-25 12:56:02 +0100

    ok

--- src/app/administrator/admin-edit/admin-edit.component.ts
+++ src/app/administrator/admin-edit/admin-edit.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-admin-edit',
@@ -7,9 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AdminEditComponent implements OnInit {
 
-  constructor() { }
+  /**
+   * ![hjh](/D:\projects\Angular\furniture\furnitureShop\mineDescriptions\Screenshot(11).png|height=400|width=700)
+   * ![hjh](./Screenshot(11).png|height=400|width=700)
+   * >>
+   * [routing with parameters]( https://www.digitalocean.com/community/tutorials/angular-query-parameters)
+   */
+  constructor(private route: ActivatedRoute) { }
 
   ngOnInit(): void {
-  }
 
+  }
 }
--- src/app/administrator/administrator-routing.module.ts
+++ src/app/administrator/administrator-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule, Route } from '@angular/router';
 import { AdministratorPageComponent } from './administrator-page/administrator-page.component';
 import { AdminProductsComponent } from './admin-products/admin-products.component';
+import { AdminEditComponent } from './admin-edit/admin-edit.component';
 
 const ADMIN_ROUTE: Route[]  = [
   {path: 'administrator', component: AdministratorPageComponent, children: [
-    {path: 'products', component: AdminProductsComponent }
+    {path: 'products', component: AdminProductsComponent},
+    {path: 'editProduct/:id' , component: AdminEditComponent}
   ]}
 ];
 
